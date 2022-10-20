6e88b27ece21ec2d8424e935bd945852741d5042
Maurice K. | Yoka <yoka57410@gmail.com>
2022-10-20 11:44:37 +0200

    styles de theme et upload d'images sur le site en prod

--- src/Service/FtpService.php
+++ src/Service/FtpService.php
@@ -54,6 +54,14 @@ class FtpService
         //Permissions for the CSS folder
          $permissions =  ftp_chmod($ftp_connection,0775,  $remote_server_dir . '/css');
    
+        //Create a /images folder on the server
+        if(!in_array('images', $files_on_server)){
+            ftp_mkdir($ftp_connection, $remote_server_dir . '/images');
+        }else{
+            //Do nothing
+        }
+        //Permissions for the images folder
+        $permissions =  ftp_chmod($ftp_connection,0775,  $remote_server_dir . '/images');
    
       
         //Upload $htmlFile to the server
@@ -63,9 +71,6 @@ class FtpService
             //echo "Error uploading $htmlFile to $ftp_host";
         }
       
-
-   
-    
        
         //Upload each file in $cssFolder to the server in the /css folder
         foreach (glob($cssFolder . '/*') as $file) {
@@ -76,7 +81,14 @@ class FtpService
             }
         }
 
-        
+        //Upload each file in $imageFolder to the server in the /images folder
+        foreach (glob($imageFolder . '/*') as $file) {
+            if (ftp_put($ftp_connection, $remote_server_dir . '/images/' . basename($file), $file, FTP_BINARY)) {
+               // echo "Successfully uploaded $file to $ftp_host as $remote_server_dir/images/" . basename($file) . '<br/>';
+            } else {
+              //  echo "Error uploading $file to $ftp_host";
+            }
+        }
         
         //Close the FTP connection
         ftp_close($ftp_connection);
@@ -105,14 +117,22 @@ class FtpService
         fwrite($file, $renderedFile);
         fclose($file);
 
-        //copy the onepagestyles.css file to the user folder
-        copy($this->local_websites_dir . '/userwebsites/default/onepagestyles.css', $miniSiteDir . '/css/defaultStyle.css');
+        //copy the onepageDefault.css file to the user folder
+        copy($this->local_websites_dir . '/public/css/onepageDefault.css', $this->local_websites_dir . '/userwebsites/default/onepageDefault.css');
+        copy($this->local_websites_dir . '/userwebsites/default/onepageDefault.css', $miniSiteDir . '/css/onepageDefault.css');
         //set the file permission to 777
-        chmod($miniSiteDir . '/css/defaultStyle.css', 0777);
+        //chmod($miniSiteDir . '/css/onepageDefault.css', 0777);
         //copy minBulma.css to the user folder
         copy($this->local_websites_dir . '/userwebsites/default/defaultBulma.min.css', $miniSiteDir . '/css/bulma.min.css');
         //set the file permission to 777
-        chmod($miniSiteDir . '/css/bulma.min.css', 0777);
+        //chmod($miniSiteDir . '/css/bulma.min.css', 0777);
+
+        //for each image in the images folder copy, it to the user folder
+        $images = glob($this->local_websites_dir . '/public/images/uploads'.$user->getUsername());
+        foreach ($images as $image) {
+            copy($image, $miniSiteDir . '/images/' . basename($image));
+        }
+
         return $renderedFile;
     }
 }
